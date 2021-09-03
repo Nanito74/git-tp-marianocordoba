0a35959f1bae4db8a4912aa46818cc7b5bde2562
Nanito74 <marianocordoba00@gmail.com>
2021-09-03 20:43:39 -0300

    Consigna del trabajo

---
+++ TPGit-MarianoCordoba.py
@@ -0,0 +1,4 @@
+#Realizar un programa que permita
+#1- Elegir la forma de pago TARJETA o EFECTIVO
+#2- Si paga con EFECTIVO tiene un desc de 10% si es superior a 1000
+#3- Si paga con TARJETA tiene un desc de 5% en cualquier monto.

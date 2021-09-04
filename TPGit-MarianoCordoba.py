4b189e4789697d2c484455ef123d8b42ce8f8126
Nanito74 <marianocordoba00@gmail.com>
2021-09-03 22:10:07 -0300

    Programa terminado

--- TPGit-MarianoCordoba.py
+++ TPGit-MarianoCordoba.py
@@ -1,8 +1,17 @@
 #Realizar un programa que permita
-#1- Elegir la forma de pago TARJETA o EFECTIVO
 monto = int(input("Ingrese el monto a abonar: "))
-formadepago = input("FORMA DE PAGO \n e- Para pagar en efectivo \n t- Para pagar con tarjeta \n")
-#2- Si paga con EFECTIVO tiene un desc de 10% si es superior a 1000
+formadepago = int(input("FORMA DE PAGO \n 1- Para pagar en efectivo \n 2- Para pagar con tarjeta \n"))
+totalfinal = 0
+if formadepago == 1:
+    if monto > 1000:
+        desc = monto * 0.1
+        totalfinal = monto - desc
+    else:
+        totalfinal = monto
+elif formadepago == 2:
+    desc = monto * 0.05
+    totalfinal = monto - desc
+else:
+    print('Tecla incorrecta')
 
-
-#3- Si paga con TARJETA tiene un desc de 5% en cualquier monto.
+print('El total final es: ', totalfinal)

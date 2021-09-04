0724d23cf6646ea2403d96f29f1fa5efe2da1f98
Nanito74 <marianocordoba00@gmail.com>
2021-09-03 21:31:04 -0300

    Inputs necesarios para el programa

--- TPGit-MarianoCordoba.py
+++ TPGit-MarianoCordoba.py
@@ -1,4 +1,8 @@
 #Realizar un programa que permita
 #1- Elegir la forma de pago TARJETA o EFECTIVO
+monto = int(input("Ingrese el monto a abonar: "))
+formadepago = input("FORMA DE PAGO \n e- Para pagar en efectivo \n t- Para pagar con tarjeta \n")
 #2- Si paga con EFECTIVO tiene un desc de 10% si es superior a 1000
+
+
 #3- Si paga con TARJETA tiene un desc de 5% en cualquier monto.
